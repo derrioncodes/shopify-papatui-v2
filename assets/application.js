fdfc0d0e00f8cc20e64424afcaac419089f26a9e
Malik Hall <malikhall247@gmail.com>
2025-10-14 20:10:53 -0600

    added footer pages

--- assets/application.js
+++ assets/application.js
@@ -369,3 +369,24 @@ function bindCartDrawerEvents() {
 document.addEventListener('DOMContentLoaded', () => {
   bindCartDrawerEvents();
 });
+
+// Accordian 
+document.addEventListener('DOMContentLoaded', function () {
+  const headers = document.querySelectorAll('.accordion-header');
+
+  headers.forEach((header) => {
+    header.addEventListener('click', function () {
+      const content = this.nextElementSibling;
+
+      // Toggle active class
+      this.classList.toggle('active');
+
+      // Slide toggle effect
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + 'px';
+      }
+    });
+  });
+});
